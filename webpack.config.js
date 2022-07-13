6ab7bf1414b4d954372edcadc3d25940d81cb6d2
Nikita Mamaev <palmally@yandex.ru>
2022-07-13 10:12:48 +0300

    create pages folder, index.js, index.css moved to pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = { 
-  entry: {main: './src/index.js'},
+  entry: {main: './pages/index.js'},
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
@@ -40,7 +40,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './index.html'
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
